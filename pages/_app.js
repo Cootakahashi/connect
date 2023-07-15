101ac056d99ce464ffb84b3c77e63c2b93232010
Cootakahashi <saiko.life2020@gmail.com>
2023-07-15 17:18:50 +0900

    remove

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,13 @@
 import "@/styles/globals.css";
 import React from "react";
-import { useRouter } from "next/router";
-import { GA4React } from "react-ga4";
-
+import Router from "next/router";
+import ReactGA from "react-ga";
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
   React.useEffect(() => {
-    const ga = new GA4React(process.env.NEXT_PUBLIC_GA_ID);
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
 
-    ga.initialize()
-      .then(() => {
-        ga.pageview(router.asPath);
-
-        router.events.on("routeChangeComplete", (url) => ga.pageview(url));
-      })
-      .catch((err) => console.error(err));
+    Router.events.on("routeChangeComplete", (url) => ReactGA.pageview(url));
   }, []);
-
   return <Component {...pageProps} />;
 }
