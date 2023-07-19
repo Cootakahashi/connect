d14ce9840f47153a2098936ac8a73a1b91943d02
Cootakahashi <saiko.life2020@gmail.com>
2023-07-20 02:30:29 +0900

    added sitemap.xml or so on

--- components/Layoutcomp.js
+++ components/Layoutcomp.js
@@ -4,9 +4,17 @@ export default function Layoutwrap({ children, metadata }) {
   return (
     <div>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="sitemap"
+          type="application/xml"
+          title="Sitemap"
+          href="/sitemap.xml"
+        />
         <title>{metadata.title}</title>
+        <meta charset="UTF-8" />
+        <meta http-equiv="Content-Language" content="ja" />
         <meta name="description" content={metadata.description} />
-
         <meta property="og:title" content={metadata.ogTitle} />
         <meta property="og:description" content={metadata.ogDescription} />
         <meta property="og:type" content="article" />
@@ -20,6 +28,7 @@ export default function Layoutwrap({ children, metadata }) {
         <meta name="twitter:image" content={metadata.ogImage} />
 
         <link rel="canonical" href={metadata.canonicalUrl} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="mx-4 md:mx-20">{children}</main>
     </div>
